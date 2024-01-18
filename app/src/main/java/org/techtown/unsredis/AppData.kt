674e4ac4c9d7227c994593a5fb231dacfc0aeff6
VITA-DELL <jhj9144@gmail.com>
2024-01-18 16:57:44 +0900

    feat viewbinding

---
+++ app/src/main/java/org/techtown/unsredis/AppData.kt
@@ -0,0 +1,26 @@
+package org.techtown.unsredis
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+
+object AppData {
+    const val ACTION_REDIS_DATA = "org.techtown.redis.data"
+
+    var isDebug = true
+    fun debug(tag: String, msg: String) {
+        if (isDebug) Log.d(tag, msg)
+    }
+
+    fun error(tag: String, msg: String) {
+        if (isDebug) Log.e(tag, msg)
+    }
+
+    private lateinit var toast: Toast
+    fun showToast(context: Context, msg: String) {
+        if (::toast.isInitialized) toast.cancel()
+        toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+}
